7f84c5688716f0e8384e513246bbb6f2e59cfe51
aucker <aucker@qq.com>
2022-04-11 22:12:14 +0800

    update the macro

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+macro_rules! count_expr {
+    () => (0);
+    ($e:expr) => (1);
+    // ($e0:expr, $e1:expr) => (1 + count_expr!($e1));
+    // ($e0:expr, $e1:expr, $e2:expr) => (1 + count_expr!($e1, $e2));
+    ($head:expr, $($tail:expr), *) => (1 + count_expr!($($tail), *));
+}
+
 macro_rules! recurrence {
-    ( a[n]:$sty:ty = $($inits:expr), + ;...; $recur:expr ) => {
+    // ( a[n]:$sty:ty = $($inits:expr), + ;...; $recur:expr ) => {
+    ( $seq:ident [$ind:ident]: $sty:ty = $($inits:expr), +; ...; $recur:expr ) => {
         // the `+` represents there is one or more repetitions here
         //
         /*
@@ -9,15 +18,17 @@ macro_rules! recurrence {
         {
             use std::ops::Index;
 
+            const MEM_SIZE: usize = count_expr!($($inits), +);
+
         struct Recurrence {
-            mem: [$sty; 2],
+            mem: [$sty; MEM_SIZE],
             pos: usize,
         }
         // This is the actual iterator type. `mem` will be the memo buffer to hold the last few values so the recurrence
         // can be computed. `pos` is to keep track of the value of `n`.
 
         struct IndexOffset<'a> {
-            slice: &'a [$sty; 2],
+            slice: &'a [$sty; MEM_SIZE],
             offset: usize,
         }
         impl<'a> Index<usize> for IndexOffset<'a> {
@@ -28,7 +39,7 @@ macro_rules! recurrence {
 
                 let index = Wrapping(index);
                 let offset = Wrapping(self.offset);
-                let window = Wrapping(2);
+                let window = Wrapping(MEM_SIZE);
 
                 let real_index = index - offset + window;
                 &self.slice[real_index.0]
@@ -50,22 +61,31 @@ macro_rules! recurrence {
         impl Iterator for Recurrence {
             type Item = $sty;
             fn next(&mut self) -> Option<$sty> {
-                if self.pos < 2 {
+                if self.pos < MEM_SIZE {
                     let next_val = self.mem[self.pos];
                     self.pos += 1;
                     Some(next_val)
                 } else {
-                    // let a = /** */;
-                    let n = self.pos;
-                    let a = IndexOffset {slice: &self.mem, offset: n};
-                    let next_val = a[n-1] + a[n-2];
+                    // // let a = /** */;
+                    // let n = self.pos;
+                    // let a = IndexOffset {slice: &self.mem, offset: n};
+                    // let next_val = a[n-1] + a[n-2];
+
+                    // we refactor this block
+                    let next_val = {
+                        // let n = self.pos;
+                        let $ind = self.pos;
+                        // let a = IndexOffset { slice: &self.mem, offset: n };
+                        let $seq = IndexOffset { slice: &self.mem, offset: $ind };
+                        $recur
+                    };
 
                     // self.mem.TODO_shuffle_down_and_append(next_val.clone());
                     {
                         use std::mem::swap;
 
                         let mut swap_tmp = next_val;
-                        for i in (0..2).rev() {
+                        for i in (0..MEM_SIZE).rev() {
                             swap(&mut swap_tmp, &mut self.mem[i]);
                         }
                     }
@@ -75,7 +95,8 @@ macro_rules! recurrence {
                 }
             }
         }
-        Recurrence {mem:[0, 1], pos: 0}
+        // Recurrence {mem:[0, 1], pos: 0}
+        Recurrence { mem: [$($inits), +], pos: 0 }
         }
     };
 }
